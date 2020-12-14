cd0748336f2cb9faf8a96fee6fde5fa7f5dd2345
dashdavid13 <davidamedina619@gmail.com>
2020-12-14 16:13:48 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Your code here
+
+
+
+  let dodger = document.getElementById("dodger");
+
+  function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+
+  function moveDodgerDown() {
+    let downNumbers = dodger.style.down.replace("px", "");
+    let down = parseInt(downNumbers, 10);
+  
+    if (down > 0) {
+      dodger.style.down = `${down - 1}px`;
+    }
+  }
+
+  function moveDodgerUp() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowDown") {
+      moveDodgerDown();
+    }
+  });
+
